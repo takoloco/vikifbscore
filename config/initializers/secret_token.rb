cf854eb8efe140ca2a3df930ceab92da7c4b04be
Tako <takoloco@takotime.com>
2012-06-30 03:23:13 +0100

    Initial app creation
    Created the app by running the "rails new" command

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vikifbscore::Application.config.secret_token = '1596202eca3dfc2393b638067706010ca70f6569df50388c63758513f16c48a31ac09cbb31f2f1d228c82b7afdbb329375b988f95ac698f25c4cc962a04fed0b'
